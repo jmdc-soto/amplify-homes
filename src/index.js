366b82afaf5c4445f3a5ac73f21b503aa8f00612
jmdc-soto <soto@jmdc.co.jp>
2022-06-09 15:58:58 +0900

    EditProfile追加

--- src/index.js
+++ src/index.js
@@ -7,12 +7,19 @@ import Amplify, { Auth } from 'aws-amplify';
 import "@aws-amplify/ui-react/styles.css";
 import {AmplifyProvider} from "@aws-amplify/ui-react";
 import awsconfig from './aws-exports';
+import { 
+  EditProfile 
+} from './ui-components';
+
+
 Amplify.configure(awsconfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <AmplifyProvider>
-      <App />
+      <App>
+        <EditProfile />
+      </App>
     </AmplifyProvider>
   </React.StrictMode>,
   document.getElementById('root')
